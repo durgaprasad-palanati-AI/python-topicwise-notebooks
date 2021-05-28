487a8fbace4f39741af3870c9bf737fd72964565
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2021-05-28 10:17:04 +0530

    if-examples

--- py-files/if-1.py
+++ py-files/if-1.py
@@ -39,3 +39,25 @@ elif num == 0:
     print("Zero")
 else:
     print("Negative number")
+
+#One-Line if Statements
+num=4
+if num==4: print('1'); print('2'); print('3')
+
+#Conditional Expressions (Python’s Ternary Operator)
+'''
+<expr1> if <conditional_expr> else <expr2>
+'''
+num=9
+number_is='even' if num%2==0 else 'odd'
+print(number_is)
+
+#The Python pass Statement
+'''
+if <expr>:
+    pass
+'''
+
+num=10
+if num==10:
+    pass #if you remove this line error occurs
