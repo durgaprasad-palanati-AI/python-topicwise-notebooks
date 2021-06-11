fa5105c52c5df4b1c1fd9eab54a2e6f060dd5ad4
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2021-06-11 21:28:08 +0530

    class with init-2ways and without init

--- py-files/class-ex-1.py
+++ py-files/class-ex-1.py
@@ -1,4 +1,6 @@
 #class with _init_
+#method-1
+print("class with _init_ ->method-1")
 class mobile:
   def __init__(self, name, price):
     self.name = name
@@ -9,7 +11,22 @@ m1 = mobile("samsung", 36000)
 print(m1.name)
 print(m1.price) 
 
+
+#class with _init_
+#method-2
+print("class with _init_ ->method-2")
+class mobile:
+  def __init__(self, name, price):
+    self.name = name
+    self.price = price
+  def details(self):
+      print("moble name is %s and price is %d"%(self.name,self.price))
+m1 = mobile("samsung", 36000)
+m1.details()
+
+
 #class without  _init_
+print("class with out _init_")
 class mobile:
   def func(self,name,price):
     print("moble name is %s and price is %d"%(name,price))
